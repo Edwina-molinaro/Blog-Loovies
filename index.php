a7aaf8e6c5e9f6515990b7ee42991f3f82449bf1
edwina.molinaro <edwina.molinaro62@gmail.com>
2020-06-15 15:18:00 +0200

    home page

--- index.php
+++ index.php
@@ -58,7 +58,15 @@
              </ul>
           </div>
         </nav>
-  
+
+        <div class="row col-12 d-flex justify-content-center mt-5 title-last_post">
+          <h2>Last post</h2>
+        </div>
+        <div class="row col-12 d-flex justify-content-center">
+        <i class="fas fa-sort-down arrow-last_post"></i>
+        </div>
+
+
       <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
