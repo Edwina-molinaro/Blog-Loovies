e5d42d9245195b6644b5cb2d4e015ee55d44dc6a
edwina.molinaro <edwina.molinaro62@gmail.com>
2020-06-19 11:03:52 +0200

    panel admin

---
+++ src/views/admin-panel.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Loovies Blog</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="../../public/css/main.css">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+</head>
+    <body>
+
+        <div class="row">
+            <div class="col-4 mt-5">
+                <h1>Loovies</h1>
+            </div>
+            <div class="col-4">
+                <h2>Dashboard</h2>
+            </div>
+            <div class="col-2">
+                <p>Go to blog</p>
+            </div>
+            <div class="col-2">
+                <p>Log out</p>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-2 panel-nav active-onglet">
+                <h3>New Post</h3>
+            </div>
+        </div> 
+
+        <div class="row">
+            <div class="col-2 panel-nav">
+                <h3>Blog Post</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-2 panel-nav">
+                <h3>Categories</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-2 panel-nav">
+                <h3>Customize</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-2 panel-nav">
+                <h3>Informations</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-2 panel-nav">
+                <h3>SETTINGS</h3>
+            </div>
+        </div>
+
+    </body>
+</html>
